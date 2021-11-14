94185d20db3cdc19b2c2bfa8d0a2cb3faabeab80
ThinkingOfNewNickname <bodyaufml@gmail.com>
2021-11-14 23:12:13 +0200

    Add main form

---
+++ LabCalculator/MainForm.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace LabCalculator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.solveButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.expressionTextBox = new System.Windows.Forms.TextBox();
+            this.expressionLabel = new System.Windows.Forms.Label();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // solveButton
+            // 
+            this.solveButton.BackColor = System.Drawing.Color.Black;
+            this.solveButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.solveButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.solveButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.solveButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
+            this.solveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.solveButton.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.solveButton.Location = new System.Drawing.Point(90, 3);
+            this.solveButton.Name = "solveButton";
+            this.solveButton.Size = new System.Drawing.Size(234, 69);
+            this.solveButton.TabIndex = 0;
+            this.solveButton.Text = "Solve";
+            this.solveButton.UseVisualStyleBackColor = false;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.resultLabel.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.resultLabel.Location = new System.Drawing.Point(365, 310);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(414, 50);
+            this.resultLabel.TabIndex = 1;
+            this.resultLabel.Text = "Result";
+            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // expressionTextBox
+            // 
+            this.expressionTextBox.BackColor = System.Drawing.Color.Black;
+            this.expressionTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.expressionTextBox.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.expressionTextBox.ForeColor = System.Drawing.Color.White;
+            this.expressionTextBox.Location = new System.Drawing.Point(365, 163);
+            this.expressionTextBox.Name = "expressionTextBox";
+            this.expressionTextBox.PlaceholderText = "How about: tg(x+PI/2)?";
+            this.expressionTextBox.Size = new System.Drawing.Size(414, 43);
+            this.expressionTextBox.TabIndex = 2;
+            // 
+            // expressionLabel
+            // 
+            this.expressionLabel.AutoSize = true;
+            this.expressionLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.expressionLabel.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.expressionLabel.Location = new System.Drawing.Point(365, 107);
+            this.expressionLabel.Name = "expressionLabel";
+            this.expressionLabel.Size = new System.Drawing.Size(414, 53);
+            this.expressionLabel.TabIndex = 3;
+            this.expressionLabel.Text = "Expression";
+            this.expressionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.BackColor = System.Drawing.Color.Black;
+            this.resultTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.resultTextBox.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.resultTextBox.ForeColor = System.Drawing.Color.White;
+            this.resultTextBox.Location = new System.Drawing.Point(365, 363);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.ReadOnly = true;
+            this.resultTextBox.Size = new System.Drawing.Size(414, 43);
+            this.resultTextBox.TabIndex = 4;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.resultTextBox, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.resultLabel, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.expressionTextBox, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.expressionLabel, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 53F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 69F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 81F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 52F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1145, 519);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.solveButton, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(365, 232);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(414, 75);
+            this.tableLayoutPanel2.TabIndex = 5;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1145, 519);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.ForeColor = System.Drawing.Color.LightGray;
+            this.MinimumSize = new System.Drawing.Size(440, 380);
+            this.Name = "MainForm";
+            this.Text = "Lab Calculator";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button solveButton;
+        private Label resultLabel;
+        private TextBox expressionTextBox;
+        private Label expressionLabel;
+        private TextBox resultTextBox;
+        private TableLayoutPanel tableLayoutPanel1;
+        private TableLayoutPanel tableLayoutPanel2;
+    }
+}
---
+++ LabCalculator/MainForm.cs
@@ -0,0 +1,10 @@
+namespace LabCalculator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
